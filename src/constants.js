b849b58dcc34c426f957e9f6baae7c0dc303f4d2
designspartanss <designspartanss@gmail.com>
2024-12-13 17:00:36 +0500

    Delete src directory

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-import profileIcon from '../src/images/profile-icon.svg'
-
-export const jobs = [
-    {id: 1, icon:profileIcon, title: 'UI/UX Designer', location: 'Seattle, USA (Remote)', date: '1 day ago', applicants: 22},
-    {id: 2, icon:profileIcon, title: 'UI/UX Designer', location: 'Seattle, USA (Remote)', date: '1 day ago', applicants: 22},
-    {id: 3, icon:profileIcon, title: 'UI/UX Designer', location: 'Seattle, USA (Remote)', date: '1 day ago', applicants: 22},
-    {id: 4, icon:profileIcon, title: 'UI/UX Designer', location: 'Seattle, USA (Remote)', date: '1 day ago', applicants: 22},
-    {id: 5, icon:profileIcon, title: 'UI/UX Designer', location: 'Seattle, USA (Remote)', date: '1 day ago', applicants: 22},
-];
